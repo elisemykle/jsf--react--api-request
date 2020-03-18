48b39fbb5a06ba25b7d3a238ee91f45703775621
Elise <elisemykle@hotmail.com>
2020-03-18 18:13:44 +0100

    Done :+1:

--- src/components/NewsArticleList.js
+++ src/components/NewsArticleList.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function NewsArticleList() {
+    const [state, setState] = useState({
+        articles: []
+    });
 
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            // call setState with the new articles, this will cause the component to re-render
+            .then(articles => setState({
+                articles: articles
+            }));
+    }, []);//!NB remember empty array to only load once
   return (
     <ul>
+      {state.articles
+        .map(article => <li key={article.id}>{article.title}</li>)
+      }
     </ul>
   );
 }
